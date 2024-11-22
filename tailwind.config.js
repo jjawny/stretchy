82525ff43fe2abb8b76def0c40281bb5c4f907ff
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-23 08:14:50 +1000

    added dot matrix background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "dot-matrix": "radial-gradient(circle, #dadada 1px, transparent 1px)",
+      },
+      backgroundSize: {
+        "dot-matrix": "30px 30px", // dot spacing
+      },
+    },
   },
   plugins: [],
 };
