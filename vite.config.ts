a7aee683eacab6554390fbdf0e28702be83ac7b7
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-23 15:54:34 +1000

    path alias (could be better)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": resolve(root),
+    },
+  },
 });
