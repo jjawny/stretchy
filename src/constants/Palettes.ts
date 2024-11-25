dd3110ce1be35f45b4b96003550735d3fc8e7386
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-25 20:47:03 +1000

    basic MUI palettes

---
+++ src/constants/Palettes.ts
@@ -0,0 +1,23 @@
+import { PaletteOptions } from "@mui/material";
+
+export const lightPalette: PaletteOptions = {
+  mode: "light",
+  background: {
+    paper: "#ffffff",
+  },
+  text: {
+    primary: "#000000",
+    secondary: "#555555",
+  },
+};
+
+export const darkPalette: PaletteOptions = {
+  mode: "dark",
+  background: {
+    paper: "#1e1e1e",
+  },
+  text: {
+    primary: "#ffffff",
+    secondary: "#aaaaaa",
+  },
+};
