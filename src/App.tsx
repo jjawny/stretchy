85bd903e35514fdcc56d48f5ee6ed068817a467b
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-25 20:48:10 +1000

    use mui theme wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import { useDynamicFontSize } from "~/hooks/useDynamicFontSize";
+import Hero from "~/Hero";
+import ThemeButton from "~/ThemeButton";
+import MuiThemeWrapper from "./MuiThemeWrapper";
 
 function App() {
-  const fontSize = useDynamicFontSize();
-
   return (
-    <main className="grid h-screen w-screen items-center justify-center">
-      <div
-        className={`flex h-[10vh] flex-col items-center justify-center justify-items-center`}
-      >
-        <h1
-          className="cursor-default select-none text-center font-syne font-extrabold text-white"
-          style={{
-            textShadow: "0 0 10px rgba(255,255,255,0.15)",
-            fontSize: `${fontSize}px`,
-            transform: "scaleY(2)",
-            lineHeight: 0.75,
-          }}
-        >
-          <span className="first-name">JOHNNY</span>{" "}
-          <span className="last-name">MADIGAN</span>
-        </h1>
-      </div>
-    </main>
+    <MuiThemeWrapper>
+      <main className="relative grid h-screen w-screen items-center justify-center">
+        <Hero />
+        <ThemeButton />
+      </main>
+    </MuiThemeWrapper>
   );
 }
 
