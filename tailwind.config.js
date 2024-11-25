215ce6f7f1d6b9282d14ee3727d7a62ad2eb69f5
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-25 20:45:19 +1000

    removed dot matrix bg (moved to MUI, see useMuiTheme hook)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "dot-matrix":
-          "radial-gradient(circle, #dadada 0.5px, transparent 0.5px)",
-      },
-      backgroundSize: {
-        "dot-matrix": "11px 11px", // dot spacing
-      },
       fontFamily: {
         syne: ["Syne", ...defaultTheme.fontFamily.serif],
       },
