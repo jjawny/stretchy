612fdaed08b943e73e9c10d9fe67d84b178c7087
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-26 11:51:37 +1000

    use custom resize hook

--- src/features/mini-hero/MiniHero.tsx
+++ src/features/mini-hero/MiniHero.tsx
@@ -1,13 +1,13 @@
 import { Tooltip } from "@mui/material";
 import { ClassValue } from "clsx";
 import { NumberSize, Resizable } from "re-resizable";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { IoArrowUndo as ResetIcon } from "react-icons/io5";
+import { DEFAULT_SIZE } from "~/features/mini-hero/mini-hero.constants";
+import { useResizeDimensions } from "~/features/mini-hero/useResizeDimensions";
 import { cn } from "~/shared/classname.utils";
 import SimpleButton from "~/shared/SimpleButton";
 import { useDynamicFontSize } from "~/shared/useDynamicFontSize";
-
-const DEFAULT_SIZE: NumberSize = { width: 320, height: 200 };
 
 type MiniHeroProps = {
   className?: ClassValue;
@@ -15,22 +15,21 @@ type MiniHeroProps = {
 
 const MiniHero: React.FC<MiniHeroProps> = (props) => {
   const { className } = props;
-  const [size, setSize] = useState<NumberSize>(DEFAULT_SIZE);
   const containerRef = useRef(null);
   const fontSize = useDynamicFontSize(containerRef);
+  const { size, handleResize, setSizeDirectly } = useResizeDimensions();
 
-  const handleResize = (e: any, direction: string, ref: HTMLElement, delta: NumberSize) => {
-    setSize((prevSize) => ({
-      width: prevSize.width + delta.width,
-      height: prevSize.height + delta.height,
-    }));
+  // Separate tech-specific Resize type from saved type
+  const mappedSize: NumberSize = {
+    width: size.width,
+    height: size.height,
   };
 
   return (
-    <Resizable size={size} minWidth={100} minHeight={100} onResizeStop={handleResize} className={cn(className)}>
+    <Resizable size={mappedSize} minWidth={100} minHeight={100} onResizeStop={handleResize} className={cn(className)}>
       <div ref={containerRef} className="relative grid h-full w-full place-content-center rounded-md bg-amber-300">
         <Tooltip title={`Reset position`}>
-          <SimpleButton onClickCallback={() => setSize(DEFAULT_SIZE)} className="absolute right-2 top-2 w-fit">
+          <SimpleButton onClickCallback={() => setSizeDirectly(DEFAULT_SIZE)} className="absolute right-2 top-2 w-fit">
             <ResetIcon />
           </SimpleButton>
         </Tooltip>
