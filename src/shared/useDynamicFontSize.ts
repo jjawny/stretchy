e295a76856a8871c57a552df23c99b496c307c5c
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-11-26 11:21:55 +1000

    dynamic font size hook now detects parent size instead

---
+++ src/shared/useDynamicFontSize.ts
@@ -0,0 +1,37 @@
+import { RefObject, useEffect, useState } from "react";
+import { debounce } from "~/shared/no-lodash.utils";
+
+const DEBOUCNE_DELAY_MS = 10;
+
+export const useDynamicFontSize = (containerRef: RefObject<HTMLElement> | null, initialSize: number = 10) => {
+  const [fontSize, setFontSize] = useState(initialSize);
+
+  useEffect(() => {
+    const updateFontSize = () => {
+      if (!containerRef?.current) return;
+      const { offsetWidth, offsetHeight } = containerRef.current;
+      const calculatedFontSize = Math.min(offsetWidth * 0.1, offsetHeight * 0.2);
+      setFontSize(calculatedFontSize);
+    };
+
+    // 1ST RENDER
+    updateFontSize();
+
+    // 👂🏻 LISTEN FOR RESIZE
+    const debouncedUpdateFontSize = debounce(updateFontSize, DEBOUCNE_DELAY_MS);
+
+    const resizeObserver = new ResizeObserver(() => {
+      debouncedUpdateFontSize();
+    });
+
+    if (containerRef?.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [containerRef]);
+
+  return fontSize;
+};
