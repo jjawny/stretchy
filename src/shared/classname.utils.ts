a6a89112f80f0706e2261c2a39f37fd57b62d348
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-12-03 21:40:28 +1000

    updated comments

--- src/shared/classname.utils.ts
+++ src/shared/classname.utils.ts
@@ -2,6 +2,6 @@ import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 /**
- * Combine ClassNames (from props and on component itself) in a type-safe & predictable way
+ * Combine ClassNames in a type-safe & predictable way
  */
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
